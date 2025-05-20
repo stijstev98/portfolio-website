29d4a90c0e2a91c59ed5dc166272f637c35eb2b4
Stijn Stevens <stijn.stevens@live.com>
2025-05-20 13:11:04 +0200

    Fix strapi config

--- strapi-cms/config/admin.ts
+++ strapi-cms/config/admin.ts
@@ -1,17 +1,17 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'generated_admin_jwt_secret_12345'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'generated_api_token_salt_12345'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'generated_transfer_token_salt_12345'),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', 'generated_encryption_key_12345'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
